084eb87d36c2affbef762a08f0cb4ae1a5aedc1d
loongxjin <chenjill099@gmail.com>
2026-04-06 18:10:39 +0800

    chore: init Go engine module and project structure

---
+++ engine/cmd/root.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "fmt"
+
+func Execute() error {
+    fmt.Println("ForkSync engine - stub implementation")
+    return nil
+}
---
+++ engine/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"os"
+	"github.com/loongxjin/forksync/engine/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+}
+}
